7bf3262e0ad651dbfd8007df0d7fd8c67917ecb1
Yermak88 <shershukovegor@gmail.com>
2026-07-22 22:15:49 +0200

    тесттесты

--- 3project.go
+++ 3project.go
@@ -65,6 +65,15 @@ func main() {
 		return
 	}
 
+	files, _ := os.ReadDir(".")
+	for _, f := range files {
+		fmt.Println("Найден файл:", f.Name())
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
 	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
 		country := r.URL.Query().Get("country")
 
